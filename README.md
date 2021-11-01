bd11feda94db7e85ba39034100c1b025b0abb5c9
YongJaeKim <kiyoja07@gmail.com>
2021-11-01 22:07:43 +0900

    3.31 Kill The Fragments

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Prismagram
 
-3.27 Introduction to Subscriptions
+3.31 Kill The Fragments
 
 Prisma 1 링크 : https://v1.prisma.io/
 
 
 
 prisma의 업데이트 적용 : yarn prisma
+query는 데이터를 읽는데(R) 사용하고, mutation은 데이터를 변조(CUD) 하는데 사용한다
--- src/fragments.js
+++ src/fragments.js
@@ -1,58 +1,60 @@
-export const USER_FRAGMENT = `
-    id
-    username
-    avatar
-`;
+// 파일 삭제
 
-export const COMMENT_FRAGMENT = `
-    id
-    text
-    user {
-        ${USER_FRAGMENT}
-    }
-`;
+// export const USER_FRAGMENT = `
+//     id
+//     username
+//     avatar
+// `;
 
-export const FILE_FRAGMENT = `
-    id
-    url
-`;
+// export const COMMENT_FRAGMENT = `
+//     id
+//     text
+//     user {
+//         ${USER_FRAGMENT}
+//     }
+// `;
 
-export const MESSAGE_FRAGMENT = `
-    id
-    text
-    to {
-        ${USER_FRAGMENT}
-    }
-    from {
-        ${USER_FRAGMENT}
-    }
-`;
+// export const FILE_FRAGMENT = `
+//     id
+//     url
+// `;
 
-export const FULL_POST_FRAGMENT = `
-    fragment PostParts on Post{
-        id
-        location
-        caption
-        files {
-            ${FILE_FRAGMENT}
-        }
-        comments {
-            ${COMMENT_FRAGMENT}
-        }
-        user {
-            ${USER_FRAGMENT}
-        }
-    }
-`;
+// export const MESSAGE_FRAGMENT = `
+//     id
+//     text
+//     to {
+//         ${USER_FRAGMENT}
+//     }
+//     from {
+//         ${USER_FRAGMENT}
+//     }
+// `;
 
-export const ROOM_FRAGMENT = `
-    fragment RoomParts on Room {
-        id
-        participants {
-            ${USER_FRAGMENT}
-        }
-        messages { 
-            ${MESSAGE_FRAGMENT}
-        }
-    }
-`;
+// export const FULL_POST_FRAGMENT = `
+//     fragment PostParts on Post{
+//         id
+//         location
+//         caption
+//         files {
+//             ${FILE_FRAGMENT}
+//         }
+//         comments {
+//             ${COMMENT_FRAGMENT}
+//         }
+//         user {
+//             ${USER_FRAGMENT}
+//         }
+//     }
+// `;
+
+// export const ROOM_FRAGMENT = `
+//     fragment RoomParts on Room {
+//         id
+//         participants {
+//             ${USER_FRAGMENT}
+//         }
+//         messages {
+//             ${MESSAGE_FRAGMENT}
+//         }
+//     }
+// `;
